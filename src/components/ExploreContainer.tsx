6889c1c3367e1103245ef8657b58f7dc1c1c1580
JuanFelipeNarvaez <jfnarvaez-2022a@corhuila.edu.co>
2024-04-06 15:41:57 -0500

    subo el login con ionic-react

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,11 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <h1></h1>
+  );
+};
+
+export default ExploreContainer;
